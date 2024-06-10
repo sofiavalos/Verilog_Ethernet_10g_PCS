be714cf0f21869ab2faf814bf949396e3832e077
sofiavalos <sofi555sofi@gmail.com>
2024-06-10 11:24:53 -0300

    add block_lock tb

---
+++ tb/eth_phy_10g_block_lock_tb.v
@@ -0,0 +1,253 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: LCD -FCEFyN
+// 
+// Create Date: 17.05.2024 10:45:39
+// Design Name: Line Loopback (LL) Setup
+// Module Name: eth_phy_10g_block_lock_tb
+// Project Name: Ethernet 10GBASE PCS Blocks
+// Description: Check block_lock conditions
+// 
+// Dependencies: eth_phy_10g
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module eth_phy_10g_block_lock_tb;
+
+    // Internal module parameters
+    parameter DATA_WIDTH = 64;
+    parameter CTRL_WIDTH = (DATA_WIDTH/8);
+    parameter HDR_WIDTH = 2;
+    parameter BIT_REVERSE = 0;
+    parameter SCRAMBLER_DISABLE = 1;
+    parameter PRBS31_ENABLE = 0;
+    parameter TX_SERDES_PIPELINE = 0;
+    parameter RX_SERDES_PIPELINE = 0;
+    parameter BITSLIP_HIGH_CYCLES = 1;
+    parameter BITSLIP_LOW_CYCLES = 8;
+    parameter COUNT_125US = 125;
+
+    // Registers to store clock and reset
+    reg rx_clk, rx_rst, tx_clk, tx_rst;
+    
+    // Input signals from xgmii encoded
+    reg [DATA_WIDTH-1:0] xgmii_txd;
+    reg [CTRL_WIDTH-1:0] xgmii_txc;
+    // Input signals from xgmii decoded
+    wire [DATA_WIDTH-1:0] xgmii_rxd;
+    wire [CTRL_WIDTH-1:0] xgmii_rxc;
+    
+    // SERDES signals for tx and rx
+    wire [DATA_WIDTH-1:0] serdes_tx_data;
+    wire [HDR_WIDTH-1:0]  serdes_tx_hdr;
+    reg [DATA_WIDTH-1:0] serdes_rx_data;
+    reg [HDR_WIDTH-1:0]  serdes_rx_hdr;
+    
+    // Error flag in the Tx
+    wire tx_bad_block;
+    
+    // Error flags in the Rx
+    wire serdes_rx_reset_req;
+    wire rx_bad_block;
+    wire rx_sequence_error;
+    wire rx_high_ber;
+    
+    // Flags in the Rx
+    wire rx_block_lock;
+    wire serdes_rx_bitslip;
+    wire rx_status;
+    reg pcs_status;
+    
+    // Error counters
+    wire [6:0] rx_error_count;
+    reg [3:0] ber_count;
+    
+    // Enable PRBS31
+    reg cfg_tx_prbs31_enable, cfg_rx_prbs31_enable;
+    
+    // Create an array with bit patterns
+    reg [63:0] test_patterns [0:5];
+    
+    // Counter
+    integer i;
+    
+    //
+    reg [5:0] sh_cnt;
+    reg [3:0] sh_invalid_cnt;
+
+    // Instance of the module PHY 10G Ethernet under test
+    eth_phy_10g #(
+        .DATA_WIDTH(DATA_WIDTH),
+        .CTRL_WIDTH(CTRL_WIDTH),
+        .HDR_WIDTH(HDR_WIDTH),
+        .BIT_REVERSE(BIT_REVERSE),
+        .SCRAMBLER_DISABLE(SCRAMBLER_DISABLE),
+        .PRBS31_ENABLE(PRBS31_ENABLE),
+        .TX_SERDES_PIPELINE(TX_SERDES_PIPELINE),
+        .RX_SERDES_PIPELINE(RX_SERDES_PIPELINE),
+        .BITSLIP_HIGH_CYCLES(BITSLIP_HIGH_CYCLES),
+        .BITSLIP_LOW_CYCLES(BITSLIP_LOW_CYCLES),
+        .COUNT_125US(COUNT_125US)
+    ) dut (
+        .rx_clk(rx_clk),
+        .rx_rst(rx_rst),
+        .tx_clk(tx_clk),
+        .tx_rst(tx_rst),
+        .xgmii_txd(xgmii_txd),
+        .xgmii_txc(xgmii_txc),
+        .xgmii_rxd(xgmii_rxd),
+        .xgmii_rxc(xgmii_rxc),
+        .serdes_tx_data(serdes_tx_data),
+        .serdes_tx_hdr(serdes_tx_hdr),
+        .serdes_rx_data(serdes_rx_data),
+        .serdes_rx_hdr(serdes_rx_hdr),
+        .serdes_rx_bitslip(serdes_rx_bitslip),
+        .serdes_rx_reset_req(serdes_rx_reset_req),
+        .tx_bad_block(tx_bad_block),
+        .rx_error_count(rx_error_count),
+        .rx_bad_block(rx_bad_block),
+        .rx_sequence_error(rx_sequence_error),
+        .rx_block_lock(rx_block_lock),
+        .rx_high_ber(rx_high_ber),
+        .rx_status(rx_status),
+        .cfg_tx_prbs31_enable(cfg_tx_prbs31_enable),
+        .cfg_rx_prbs31_enable(cfg_rx_prbs31_enable)
+    );
+    
+    event terminate_sim;
+    
+    always #10 rx_clk = ~rx_clk;
+    always #10 tx_clk = ~tx_clk; 
+    
+    initial
+        begin: TEST_CASE
+            // Initialize clock and reset signals
+            rx_rst = 1;
+            rx_clk = 0;
+            tx_rst = 1;
+            tx_clk = 0;
+            
+            // Assign patterns to the array
+            test_patterns[0] = 64'hFFFFFFFFFFFFFFFF; 
+            test_patterns[1] = 64'h0000000000000000; 
+            test_patterns[2] = 64'h5555555555555555; 
+            test_patterns[3] = 64'hAAAAAAAAAAAAAAAA; 
+            test_patterns[4] = 64'hFEFEFEFEFEFEFEFE; 
+            test_patterns[5] = 64'h0707070707070707; 
+            
+            // Initialize the error flags to 0
+            pcs_status = 1'b0;
+            
+            // Disable PRBS31
+            cfg_tx_prbs31_enable = 0;
+            cfg_rx_prbs31_enable = 0;
+
+            // Set Reset to 0
+            #10 
+            rx_rst = 0;
+            tx_rst = 0;
+            
+            // Initialize monitors
+            $display("\n ---------Starting simulation---------");
+            $monitor("Time: %0t | block lock: %0d", $time, rx_block_lock);
+            $monitor("Time: %0t | high_ber: %0d", $time, rx_high_ber);
+            $monitor("Time: %0t | bitslip: %0d", $time, serdes_rx_bitslip);
+            $monitor("Time: %0t | PCS status: %0d", $time, pcs_status);
+            $monitor("Time: %0t | ber count: %0d", $time, ber_count);
+            // Inicializa la data del tx
+            xgmii_txc = 8'hxx;
+            xgmii_txd = 64'hxxxxxxxxxxxxxxxx;
+           
+            // Configura encabezado inicial
+            #20
+            serdes_rx_hdr <= 2'h2;
+            
+            // Envia 3 encabezados invalidos
+            #1470
+            serdes_rx_hdr <= 2'h3;
+            #60
+            // Envia 13 encabezados validos
+            serdes_rx_hdr <= 2'h2;
+            #260
+            // Envia 3 encabezados invalidos          
+            serdes_rx_hdr <= 2'h3;
+            #60
+            // Envia 13 encabezados validos
+            serdes_rx_hdr <= 2'h2;
+            #260
+            // Envia 3 encabezados invalidos
+            serdes_rx_hdr <= 2'h3;
+            #60
+            // Envia 13 encabezados validos
+            serdes_rx_hdr <= 2'h2;
+            #260
+            // Envia 3 encabezados invalidos
+            serdes_rx_hdr <= 2'h3;
+            #60
+            // Envia 13 encabezados validos
+            serdes_rx_hdr <= 2'h2;
+            #260
+            // Envia 15 encabezados invalidos
+            serdes_rx_hdr <= 2'h3;
+            #300
+            // Envia 49 encabezados validos
+            serdes_rx_hdr <= 2'h2;
+            #980
+            // Envia 16 encabezados invalidos
+            serdes_rx_hdr <= 2'h3;
+            #320
+            // Envia 1 encabezado valido
+            serdes_rx_hdr <= 2'h2;
+            #20
+            // Envia 1 encabezado invalido
+            serdes_rx_hdr <= 2'h3;
+            #20
+            // Envia 1 encabezado validos
+            serdes_rx_hdr <= 2'h2;
+            #20
+            // Envia 1 encabezado invalido
+            serdes_rx_hdr <= 2'h3;
+            #20
+            // Envia encabezados validos
+            serdes_rx_hdr <= 2'h2;
+            
+           
+        end 
+        
+        
+        always @(posedge tx_clk) begin
+            for (i = 0; i < 6; i = i + 1) begin
+                serdes_rx_data <= test_patterns[i];    
+            end
+        end
+        
+        
+        always @(posedge rx_clk) begin
+            ber_count <= dut.eth_phy_10g_rx_inst.eth_phy_10g_rx_if_inst.eth_phy_10g_rx_ber_mon_inst.ber_count_reg;
+            sh_cnt <= dut.eth_phy_10g_rx_inst.eth_phy_10g_rx_if_inst.eth_phy_10g_rx_frame_sync_inst.sh_count_reg;
+            sh_invalid_cnt <= dut.eth_phy_10g_rx_inst.eth_phy_10g_rx_if_inst.eth_phy_10g_rx_frame_sync_inst.sh_invalid_count_reg;
+            if(rx_block_lock && !rx_high_ber) begin
+                pcs_status = 1'b1;
+            end else begin
+                pcs_status = 1'b0;
+            end
+        end
+        
+        always @(terminate_sim) begin
+            $display("\n ---------Final state---------");
+            $display("block lock: %0d", rx_block_lock);
+            $display("high_ber: %0d", rx_high_ber);
+            $display("bitslip: %0d", serdes_rx_bitslip);
+            $display("PCS status: %0d", pcs_status);
+            $display("ber count: %0d", ber_count);
+            
+            #5 $finish;
+        end  
+       
+endmodule
+
